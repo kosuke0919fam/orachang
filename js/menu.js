82d0a28e6252f379a991f6bef50ac0781d9aa89c
LAPTOP-8QQR8EKM\k <kosuke0919fam@gmail.com>
2025-04-13 22:23:52 +0900

    Site updated: 2025-04-13 22:23:52

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const nav = document.querySelector(".nav-links");
-  if (toggle && nav) {
-    toggle.addEventListener("click", function () {
-      nav.classList.toggle("menu-open");
-    });
-  }
-});

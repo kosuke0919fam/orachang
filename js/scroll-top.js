174d951713f743f6db4348816376d020a134fef5
LAPTOP-8QQR8EKM\k <kosuke0919fam@gmail.com>
2025-05-05 18:23:57 +0900

    Site updated: 2025-05-05 18:23:57

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,8 +1,11 @@
+// source/js/scroll-top.js
 window.addEventListener('load', () => {
-  if (window.location.href.match(/page\/\d+/)) {
+  if (window.location.href.match(/page\/\d+/) || window.location.hash.includes('#page')) {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 });
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
   if (window.location.hash.includes('#page')) {

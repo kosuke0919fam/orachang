ccedc02279913ef474bf3e40245f05b4f8a99edf
LAPTOP-8QQR8EKM\k <kosuke0919fam@gmail.com>
2025-05-20 22:41:32 +0900

    Site updated: 2025-05-20 22:41:31

--- js/scroll-to-top-on-pagination.js
+++
@@ -1,7 +0,0 @@
-if (typeof window !== 'undefined') {
-  window.addEventListener('load', () => {
-    if (location.href.includes('#page')) {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-  });
-}
--- js/scroll-top.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  // ユーザーが明示的にページ番号へ遷移した場合のみスクロール
-  const path = window.location.pathname;
-  const isPaged = /\/page\/\d+\/?$/.test(path);
-
-  if (isPaged) {
-    window.scrollTo({ top: 0, behavior: 'auto' }); // instant scroll
-  }
-});

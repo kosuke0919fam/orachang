349a1d66de17ab71585c8819733266deb33e588a
LAPTOP-8QQR8EKM\k <kosuke0919fam@gmail.com>
2025-06-27 22:04:46 +0900

    Site updated: 2025-06-27 22:04:46

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
+const container = require('markdown-it-container');
+
+hexo.extend.filter.register('markdown-it:renderer', function (md) {
+  md.use(container, 'note', {
+    render(tokens, idx) {
+      const token = tokens[idx];
+      return token.nesting === 1 ? '<div class="note-block">' : '</div>';
+    }
+  });
+
+  md.use(container, 'thread', {
+    render(tokens, idx) {
+      return tokens[idx].nesting === 1 ? '<div class="thread-block">' : '</div>';
+    }
+  });
+
+  // :::twitter
+  md.use(container, 'twitter', {
+    render: (tokens, idx) => tokens[idx].nesting === 1 ? '<div class="twitter-post">\n' : '</div>\n'
+  });
+
+  // :::dm
+  md.use(container, 'dm', {
+    render: (tokens, idx) => tokens[idx].nesting === 1 ? '<div class="dm-bubble dm-twitter">\n' : '</div>\n'
+  });
+
+  // :::minutes
+  md.use(container, 'minutes', {
+    render: (tokens, idx) =>
+      tokens[idx].nesting === 1
+        ? '<table class="minutes-table"><tbody>\n'
+        : '</tbody></table>\n'
+  });
+
+  // :::section
+  md.use(container, 'section', {
+    render: (tokens, idx) =>
+      tokens[idx].nesting === 1 ? '<section>\n' : '</section>\n'
+  });
+
+  // 画像→<figure><figcaption>変換
+  const defaultImageRender = md.renderer.rules.image;
+  md.renderer.rules.image = function (tokens, idx) {
+    const token = tokens[idx];
+    const src = token.attrs[token.attrIndex('src')][1];
+    const alt = token.content;
+    return `<figure class="image-frame">
+  <img src="${src}" alt="${alt}">
+  <figcaption>${alt}</figcaption>
+</figure>`;
+  };
+});
 console.log("📏 canvas init");
 
 window.addEventListener("load", () => {
@@ -10,24 +62,14 @@ window.addEventListener("load", () => {
       return;
     }
 
-    // canvasサイズを noteBlock に合わせる
+    const style = window.getComputedStyle(text);
+    const lineHeight = parseFloat(style.lineHeight);
+    const paddingTop = parseFloat(style.paddingTop || 0);
+    const paddingBottom = parseFloat(style.paddingBottom || 0);
     const totalHeight = text.offsetHeight;
-    canvas.width = noteBlock.clientWidth;
-    canvas.height = totalHeight;
 
-    const ctx = canvas.getContext("2d");
-    ctx.strokeStyle = "#c8b798"; // 線の色
-    ctx.lineWidth = 1;
+    console.log({ lineHeight, paddingTop, paddingBottom, totalHeight });
 
-    const baseTop = text.getBoundingClientRect().top;
-    const rects = text.getClientRects();
+    const lines = Math.floor((totalHeight - paddingTop - paddingBottom) / lineHeight);
 
-    for (const rect of rects) {
-      const bottomY = rect.bottom - baseTop + 0.5; // 行の下端にオフセット調整
-      ctx.beginPath();
-      ctx.moveTo(0, bottomY);
-      ctx.lineTo(canvas.width, bottomY);
-      ctx.stroke();
-    }
-  });
-});
+
